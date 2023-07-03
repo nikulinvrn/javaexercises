a67cb445ffc5734be63fe66b2079f4c0154597bf
Max Krot <imrkrot@gmail.com>
2023-07-03 09:31:18 +0300

    Задача на поиск пары в массиве, образующей целевую сумму. Постановка задачи.

---
+++ jurievLessons/TwoSum.java
@@ -0,0 +1,10 @@
+package jurievLessons;
+
+//Задача: из введенных чисел (массив) вычленить пару, образующую целевую сумму (вводится пользователем)
+//Пример резулььтатов выполнения
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+public class TwoSum {
+
+}
