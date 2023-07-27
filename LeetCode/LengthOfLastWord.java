e55f8c914368076d2f3a8d2b3e1ec78057ee15f3
Nikulin <nikulin.vrn@bk.ru>
2023-07-27 17:30:01 +0300

    LCTask #58 Length of last word is complete.

--- LeetCode/LengthOfLastWord.java
+++ LeetCode/LengthOfLastWord.java
@@ -1,12 +1,14 @@
 /* 58. Длина последнего слова.
  *
- * Дана строка, состоящая из нескольких слов или предложений. Необходимо найти наибольшую подстроку в конце между пробелами.
+ * Дана строка, состоящая из нескольких слов или предложений. Необходимо найти длину наибольшей
+ * подстроки в конце между пробелами.
  *
  * Ограничения:
  *     - строка обязательно содержит хотябы одно слово
  *     - длина строки не более 10000 символов
  *     - строка содержит только английские буквы и пробелы
  *
+ * Выводы по итогам: есть вариант еще проще, через s.split(" ") - просто считаем длину последнего слова.
  */
 
 package LeetCode;
@@ -14,11 +16,21 @@ package LeetCode;
 public class LengthOfLastWord {
 
     public static void main(String[] args) {
-        //This is main
+        //This is test's
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+        System.out.println(lengthOfLastWord("a"));
+        System.out.println(lengthOfLastWord("    day"));
     }
 
-    public int lengthOfLastWord(String s) {
+    public static int lengthOfLastWord(String s) {
+        int lengthOfLastWord = 0;
+        String cleanS = s.trim();
+        int i = cleanS.length() - 1;
+        while(i >= 0 && cleanS.charAt(i) != ' ') {
+            lengthOfLastWord++;
+            i--;
+        }
 
-        return 0;
+        return lengthOfLastWord;
     }
 }
