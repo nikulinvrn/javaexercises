2aa118cd0ef145bb2a7492e1d95692056649b188
Nikulin <nikulin.vrn@bk.ru>
2023-07-28 11:39:04 +0300

    LCTask #80 Remove duplicates from sorted array II is complete.

--- LeetCode/RemoteDuplicatesFromSortedArrayII.java
+++ LeetCode/RemoteDuplicatesFromSortedArrayII.java
@@ -13,12 +13,25 @@ package LeetCode;
 public class RemoteDuplicatesFromSortedArrayII {
     public static void main(String[] args) {
         // Write test here
-
+        System.out.println(removeDuplicates(new int[]{1, 1, 1, 2, 2, 3}));
+        System.out.println(removeDuplicates(new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3}));
+//        System.out.println(removeDuplicates(new int[]{}));
+//        System.out.println(removeDuplicates(new int[]{}));
     }
 
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
+        int cursor = 1;
+        for (int index = 2; index < nums.length; index++){
+            if (nums[index] != nums[cursor - 1]){
+                nums[++cursor] = nums[index];
+            }
+        }
 
+        for (int x: nums) {
+            System.out.print(" [" + x + "] ");
+        }
 
-        return 0;
+        return cursor + 1;
     }
+
 }
